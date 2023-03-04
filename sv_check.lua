0b4b3d84434ba9af6af1c6f5f1dc89d9a602f1df
YoCazza <zwiesereijnmichael@gmail.com>
2023-03-04 19:13:53 +0100

    Added security feature
    Ad

--- sv_check.lua
+++ sv_check.lua
@@ -6,3 +6,15 @@ AddEventHandler("ch_checkperms:isAllowed", function()
         TriggerClientEvent("ch_checkperms.returnIsAllowed", source, false)
     end
 end)
+
+-- If you have changed the folder name you can delete the code below.
+AddEventHandler('onResourceStart', function(resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then
+      return
+    end
+    Wait(5000)
+    fnames = "CH-Blacklist", "Blacklist", "blacklist", "black-list"
+    if resourceName == fnames then
+      print("[^1WARNING^0] : We recommend changing the folder name, hackers can easily view folders and stop them, change the folder name to something unique that will not be noticed.")
+    end
+  end)
