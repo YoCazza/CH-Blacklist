06934e7bf2386c45a0b7174fb7024f7029211ab2
YoCazza <zwiesereijnmichael@gmail.com>
2023-03-05 00:27:48 +0100

    Security feature thanks to hmaxnl

--- sv_check.lua
+++ sv_check.lua
@@ -7,14 +7,16 @@ AddEventHandler("ch_checkperms:isAllowed", function()
     end
 end)
 
--- If you have changed the folder name you can delete the code below.
+-- If you have changed the folder name and you dont see any warnings you can delete the code below.
 AddEventHandler('onResourceStart', function(resourceName)
-    if (GetCurrentResourceName() ~= resourceName) then
-      return
-    end
-    Wait(5000)
-    fnames = "CH-Blacklist", "Blacklist", "blacklist", "black-list", "CH-Blacklist-main"
-    if resourceName == fnames then
+  if (GetCurrentResourceName() ~= resourceName) then
+    return
+  end
+  fnames = {'CH-Blacklist', 'Blacklist', 'blacklist', 'black-list', 'CH-Blacklist-main', 'ch-blacklist'}
+  Wait(5000)
+  for i, fname in ipairs(fnames) do
+  if resourceName == fname then
       print("[^1WARNING^0] : We recommend changing the folder name, hackers can easily view folders and stop them, change the folder name to something unique that will not be noticed.")
     end
-  end)
+  end
+end)
