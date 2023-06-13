c081897f705157b141d96a12e9d4d653f74b1a24
YoCazza <101638151+YoCazza@users.noreply.github.com>
2023-06-13 09:53:03 +0200

    Push version 1.3

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 author 'YoCazza#0001'
 description 'Blacklist vehicles and weapons on your FiveM Server!'
-version '1.2'
+version '1.3'
 lua54 'yes'
 
 client_scripts {
