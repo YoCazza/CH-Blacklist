d9314cb607de80a7b24a38df275e254ffd25a98e
YoCazza <101638151+YoCazza@users.noreply.github.com>
2023-08-03 10:25:10 +0200

    Push version 1.4

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,17 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
-author 'YoCazza#0001'
+author 'YoCazza'
 description 'Blacklist vehicles and weapons on your FiveM Server!'
-version '1.3'
+version '1.4'
 lua54 'yes'
 
-client_scripts {
-	"config.lua",
-	"cl_vehicles.lua",
-	"cl_weapons.lua"
-}
-
-server_scripts {
-	"config.lua",
-	"sv_check.lua"
-}
+client_scripts {"config.lua", "client.lua"}
+server_scripts {"config.lua", "server.lua"}
