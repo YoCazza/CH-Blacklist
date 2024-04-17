d388881f5494d50b0f4b9fabe595d915a21b2ffe
YoCazza <101638151+YoCazza@users.noreply.github.com>
2024-04-17 18:29:51 +0200

    Version 2.0
    Less code, more efficient

--- server.lua
+++ server.lua
@@ -1,33 +1,17 @@
--- Check ace permissions
-RegisterServerEvent("ch_checkperms_veh:isAllowed")
-AddEventHandler("ch_checkperms_veh:isAllowed", function()
-    if IsPlayerAceAllowed(source, CH.AcePermsVehicle) then
-        TriggerClientEvent("ch_checkperms_veh.returnIsAllowed", source, true)
-    else
-        TriggerClientEvent("ch_checkperms_veh.returnIsAllowed", source, false)
-    end
+RegisterServerEvent("blacklist:isAllowed")
+AddEventHandler("blacklist:isAllowed", function()
+    TriggerClientEvent("blacklist:isAllowed:return", source, IsPlayerAceAllowed(source, CH.AceName))
 end)
 
-RegisterServerEvent("ch_checkperms_wep:isAllowed")
-AddEventHandler("ch_checkperms_wep:isAllowed", function()
-    if IsPlayerAceAllowed(source, CH.AcePermsWeapon) then
-        TriggerClientEvent("ch_checkperms_wep.returnIsAllowed", source, true)
-    else
-        TriggerClientEvent("ch_checkperms_wep.returnIsAllowed", source, false)
-    end
-end)
+-- Remove this code if you have changed the folder name.
+local blacklistNames = {'CH-Blacklist', 'Blacklist', 'blacklist', 'CH-Blacklist-main'}
 
--- If you already changed the folder name you can remove this code.
-local fnames = {'CH-Blacklist', 'Blacklist', 'blacklist', 'CH-Blacklist-main'}
 AddEventHandler('onResourceStart', function(resourceName)
-    if GetCurrentResourceName() ~= resourceName then
-        return
-    end
-    Wait(5000)
-    for i, fname in ipairs(fnames) do
-        if resourceName == fname then
-            print("[^1WARNING^0]: The folder name '" .. resourceName .. "' contains the word 'blacklist'.")
-            print("[^1WARNING^0]: We recommend changing the folder name to something unique that will not be noticed.")
+    if GetCurrentResourceName() ~= resourceName then return end
+    Citizen.Wait(5000)
+    for _, name in ipairs(blacklistNames) do
+        if resourceName == name then
+            print("^1[WARNING]^0: The folder name '" .. resourceName .. "' contains 'blacklist'.\nWe recommend changing the folder name to something unique that won't attract attention.")
             break
         end
     end
